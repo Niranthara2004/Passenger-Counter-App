6d0339c2a49b12ebbfe4a9ffae4265fd4eac54a1
Niranthara2004 <156901346+Niranthara2004@users.noreply.github.com>
2024-12-28 14:55:33 +0530

    Second Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+// let saveEl = document.getElementById("save-el")
+// let count = 0
+// //camelCase
+// let countEl= document.getElementById("count-el")
+
+// function increment(){
+//   count += 1
+//   countEl.textContent=count
+// }
+
+// function save(){
+//     let saveVar = count + '-'
+//     saveEl.textContent += saveVar
+//     countEl.textContent = 0
+//     count = 0
+// }
+
+
+
+
+// let firstName = "Adithya"
+// let lastName = "Niranthara"
+
+// let fullName = firstName + lastName 
+
+// console.log(fullName)
+
+
+
+let name = "Linda"
+let greeting = "Hi There"
+
+function call(){
+  let words = name + + greeting 
+}
+
+call(words)
